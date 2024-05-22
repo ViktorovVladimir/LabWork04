3424e05c256d6ed2d7056cd2c8adc74d26444856
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-22 21:26:38 +0300

    add ex.3
    - добавил проект упражнения №3: использование возвращаемых параметров в методах

---
+++ Lab05/Starter/Utility03/Utility/Test.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace UtilsNameSpace02
+{
+    using System;
+
+    //--.
+    public class Test
+    {
+        public static void Main()
+        {
+            //--. Get input numbers
+            Console.WriteLine("Enter first number: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int y = int.Parse(Console.ReadLine());
+
+            //--. Test the Greater() method
+            Console.WriteLine("Before swap: " + x + ", " + y);
+            Utils.Swap(ref x, ref y);
+            Console.WriteLine("After swap: " + x + ", " + y);
+        }
+    }
+}
---
+++ Lab05/Starter/Utility03/Utility/Utils.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+namespace UtilsNameSpace02
+{
+    using System;
+
+    //--.
+    class Utils
+    {
+        
+        //--.
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
