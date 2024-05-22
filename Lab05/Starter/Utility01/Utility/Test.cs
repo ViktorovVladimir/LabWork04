a5b29e2147755c9f72132eed56c2ebe571a0ba9a
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-22 21:09:48 +0300

    Add Ex.2 and update Ex.1
    - добавил упражнение №2: использование в методах параметров, передаваемых по ссылке;
    - обновил программу упражнения №1

---
+++ Lab05/Starter/Utility01/Utility/Test.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace UtilsNameSpace01
+{
+    using System;
+
+    //--.
+    public class Test
+    {
+        public static void Main()
+        {
+            //--. Get input numbers
+            Console.WriteLine("Enter first number: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int y = int.Parse(Console.ReadLine());
+
+            //--. Test the Greater() method
+            int greater = Utils.Greater(x, y);
+            Console.WriteLine("The greater value is " + greater);
+
+        }
+
+
+
+    }
+}
---
+++ Lab05/Starter/Utility01/Utility/Utils.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+namespace UtilsNameSpace01
+{
+    using System;
+
+    class Utils
+    {
+        
+        //--.
+        public static int Greater(int a, int b)
+        {
+            if(a > b) 
+            { 
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+
+    }
+
+
+}
